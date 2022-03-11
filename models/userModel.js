8d6e45a1bc85d5c34ae1ae25d90423680cacaf9c
Zain Javed <zainjhere@gmail.com>
2022-03-12 01:20:30 +0500

    Bcrypt password hash

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -27,5 +28,22 @@ const userSchema = new mongoose.Schema({
 }, {
     timestamps: true
 });
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  try {
+    return await bcrypt.compare(enteredPassword, this.password);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  } 
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password,salt);
+})
 
 module.exports = mongoose.model('User', userSchema);
