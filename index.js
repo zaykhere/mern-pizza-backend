a83d14a60dbb32cf8cd767d273dd78fe6b96dfdf
Zain Javed <zainjhere@gmail.com>
2022-08-09 15:54:40 +0500

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -8,6 +9,7 @@ const app = express();
 dotenv.config({ path: "./config.env" });
 
 app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect(process.env.DB_CONN, {
